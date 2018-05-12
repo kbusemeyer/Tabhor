f69612ced6bbb0902574f7d183874906eea01dce
kbusemeyer <busemeyer.k@husky.neu.edu>
2018-05-12 18:01:12 -0400

    get urls to update accordingly

--- background.js
+++ background.js
@@ -3,22 +3,30 @@ var add = (function () {
     var urlHash = [];
     //console.log(urlHash);
     return { 
-    	addUrl: function(tabId, tabInfo) {
+    	//adds the original url when a tab is created
+    	addUrl: function(tabInfo) {
     		var tab = {
-    			id: tabId.toString(),
+    			id: tabInfo.id.toString(),
     			url: (findEndOfUrl(tabInfo.url) === -1) ? tabInfo.url : tabInfo.url.substring(0, findEndOfUrl(tabInfo.url)),
-    			openTime: new Date;
+    			openTime: new Date
     		};
     		urlHash.push(tab);
     		console.log(tab.url);
     		return urlHash.tab;
     	},
-    	updateUrl: function(tabId, tabInfo) {
-
-    	}
+    	//updates the tab url when it changes
+    	updateUrl: function(tabId, tabUrl) {
+    		for (i = 0; i < urlHash.length; i++) {
+    			if (urlHash[i].id === tabId.toString()) {
+    				urlHash[i].url = tabUrl;
+    			}
+    		}
+    	},
+    	//returns the urlHash
     	getUrlHash: function() {
     		return urlHash;
     	},
+    	//adds the tabs that are open when the extension is initially installed/updated
     	addCurOpenUrl: function(tabsInfo) {
     		for (i = 0; i < tabsInfo.length; i++) {
     			urlHash.push(tabsInfo[i]);
@@ -64,15 +72,15 @@ chrome.runtime.onInstalled.addListener(function(details) {
 
 //when tab is created, get the tab id and set the date
 chrome.tabs.onCreated.addListener(function(tabInfo) {
-	addStartTime(tabInfo.id);
-	add.addUrl(tabInfo);
+	//addStartTime(tabInfo.id);
+	add.addUrl(tabInfo)
 });
 
 
 //when tab is updated - change the url associated with the tabId to the new one
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tabInfo) {
 	if (changeInfo.status === 'complete') {
-		add.addUrl(tabId, tabInfo);
+		add.updateUrl(tabId, tabInfo.url);
 	};
 });
 
