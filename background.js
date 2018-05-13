a5de6bad900364eecbe49a492630d5ff19da1d97
kbusemeyer <busemeyer.k@husky.neu.edu>
2018-05-13 17:32:42 -0400

    changed tab structure and got remove urls to work

--- background.js
+++ background.js
@@ -1,101 +1,126 @@
 //adds the url to the urlHash
-var add = (function () {
-    var urlHash = [];
+var tabMethods = (function () {
+    var urlMap = new Map();
     //console.log(urlHash);
     return { 
     	//adds the original url when a tab is created
     	addUrl: function(tabInfo) {
-    		var tab = {
-    			id: tabInfo.id.toString(),
-    			url: (findEndOfUrl(tabInfo.url) === -1) ? tabInfo.url : tabInfo.url.substring(0, findEndOfUrl(tabInfo.url)),
-    			openTime: new Date
-    		};
-    		urlHash.push(tab);
-    		console.log(tab.url);
-    		return urlHash.tab;
-    	},
-    	//updates the tab url when it changes
-    	updateUrl: function(tabId, tabUrl) {
-    		for (i = 0; i < urlHash.length; i++) {
-    			if (urlHash[i].id === tabId.toString()) {
-    				urlHash[i].url = tabUrl;
-    			}
-    		}
+    		urlMap.set(tabInfo.id, tabInfo.url);
+    		return tabInfo.id;
     	},
     	//returns the urlHash
-    	getUrlHash: function() {
-    		return urlHash;
+    	getUrlMap: function() {
+    		return urlMap;
     	},
     	//adds the tabs that are open when the extension is initially installed/updated
-    	addCurOpenUrl: function(tabsInfo) {
+    	addCurOpenUrl: function(tabsInfo, tabIds) {
     		for (i = 0; i < tabsInfo.length; i++) {
-    			urlHash.push(tabsInfo[i]);
+    			urlMap.set(tabsInfo[i][0], tabsInfo[i][1]);
     		};
-    		return urlHash;
+    		return urlMap;
+    	},
+    	removeUrl: function(curTabInfo, curTabIds) {
+    		//need to run through list and see if any of the id's have been removed
+    		for (var id of urlMap.keys()) {
+    			if (!(curTabIds.includes(id))) {
+    				urlMap.delete(id);
+    				//console.log(curTabIds.includes(id));
+    			}
+    			else {
+    				continue;
+    			}
+    		}
+
+/*
+    		for (i = 0; i < ids.size; i++) {
+    			console.log("here");
+    			if (!(curTabIds.includes(ids[i]))) {
+    				urlMap.delete(ids[i]);
+    			}
+    			else {
+    				continue;
+    			}
+    		}
+    		*/
+    		return urlMap;
     	}
     };
 })();
 
-function getCurTabIds(callback) {
+function getCurTabs(callback) {
   
   var queryInfo = {
   };
 
   chrome.tabs.query(queryInfo, function(tabs) {
       var tabsInfo = [];
+      var ids = [];
 
     tabs.forEach(function(tab) {
-    	var addTab = {
-    		id: tab.id.toString(),
-    		url: tab.url
-    	};
+    	var addTab = [];
+    	ids.push(tab.id);
+    	addTab.push(tab.id, findEndOfUrl(tab.url));
       	tabsInfo.push(addTab);
     });
-
-    callback(tabsInfo);
+    callback(tabsInfo, ids);
   });
 };
 
+//checks to see if any of the common website ends are found to shorten url
 function findEndOfUrl(url) {
-	var ind = url.indexOf(".com");
-	return ind;
+	//.net, .org .com
+	if (url.includes(".com")) {
+		return url.substring(0, url.indexOf(".com") + 4);
+	}
+	else if (url.includes(".org")) {
+		return url.substring(0, url.indexOf(".org") + 4);
+	}
+	else if (url.includes(".net")) {
+		return url.substring(url.indexOf(".net") + 4);
+	}
+	else {
+		return url;
+	}
 }
 
 //when the app is first installed, add the currently open tabs to the tabHash
 chrome.runtime.onInstalled.addListener(function(details) {
 	//want this to happen everytime? users can't reload?
 	if (details.reason === "install" || details.reason === "update") { 
-		getCurTabIds(add.addCurOpenUrl);
+		getCurTabs(tabMethods.addCurOpenUrl);
 	};
 	//need to add start time too
 });
 
 //when tab is created, get the tab id and set the date
 chrome.tabs.onCreated.addListener(function(tabInfo) {
-	//addStartTime(tabInfo.id);
-	add.addUrl(tabInfo)
+	addStartTime(tabInfo.id);
+	tabMethods.addUrl(tabInfo)
 });
-
 
 //when tab is updated - change the url associated with the tabId to the new one
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tabInfo) {
+
 	if (changeInfo.status === 'complete') {
-		add.updateUrl(tabId, tabInfo.url);
+		tabMethods.addUrl(tabInfo);
 	};
 });
 
-/*
-//sets the current timestamp in storage
-function addStartTime(tabID) {
-	var firstOpened = new Date;
-	var stringId = tabID.toString();
-	chrome.storage.sync.set({stringId : firstOpened}, function() {
-		console.log(tabID + ": " + firstOpened)
-	})
-};
-*/
+//tab changes when closed - this becomes a problem when allowing them to close
+// from the app because won't be able to
+chrome.tabs.onActiveChanged.addListener(function(tabId, selectInfo) {
+	getCurTabs(tabMethods.removeUrl);
+});
 
-//after .com, then nix
+ //sets the current timestamp in storage
+ function addStartTime(tabID) {
+ 	var firstOpened = new Date;
+ 	var stringId = tabID.toString();
+ 	chrome.storage.sync.set({stringId : firstOpened}, function() {
+ 		//var urlList = document.getElementById("urlList");
+ 		console.log(tabID + " " + firstOpened)
+ 	})
+ };
 
 
 
--- popup.js
+++ popup.js
@@ -1,28 +1,12 @@
-//this is getting the tab ids for all the currently open ids soooo need to fix that later
-function getAllTabIds(callback) {
-
-  var queryInfo = {
-  };
-
-  chrome.tabs.query(queryInfo, function(tabs) {
-      var ids = [];
-
-    tabs.forEach(function(tab) {
-      ids.push(tab.id);
-    })
-
-    callback(ids);
-  });
-};
-
 function addTabToList() {
   var backgroundPage = chrome.extension.getBackgroundPage();
-  var tabHash = backgroundPage.add.getUrlHash();
+  var tabMap = backgroundPage.tabMethods.getUrlMap();
 
   var tabList = document.getElementById("tabList");
-  for (i = 0; i < tabHash.length; i++) {
-    var id = tabHash[i].id;
-    var url = tabHash[i].url;
+
+  for (var id of tabMap.keys()) {
+    console.log("here");
+    var url = tabMap.get(id);
     var li = document.createElement("li"); //create a list element
     li.setAttribute("id", id); //set list element to tabId to access later
     li.appendChild(document.createTextNode(url)); //add url to text
@@ -31,6 +15,5 @@ function addTabToList() {
 };
 
 document.addEventListener('DOMContentLoaded', function() {
-  //getAllTabIds(addTabToList);
   addTabToList();
 });
